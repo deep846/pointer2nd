154b642f5ac40a5525ed677aa4cc2c6b0814acd1
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-01 02:46:10 +0530

    pointer 2nd

---
+++ Pointer 2nd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main()
+{
+    int *x,*y,*z
+    x=(int*)malloc(sizeof(int));
+    y=(int*)malloc(sizeof(int));
+    z=(int*)malloc(sizeof(int));
+    printf(" Enter the value of a & b = \n")
+    scanf("%d%);
+    *z=*x+*y;
+    printf("\nthe sum of the variable a & b is = %d",*z);
+    
+    
+}
